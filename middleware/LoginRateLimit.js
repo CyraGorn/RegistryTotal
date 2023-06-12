10d489ae116ce82486f37c7c084872a2a7ce2ae6
CyraGorn <bao.ngoquy03@gmail.com>
2023-06-12 22:35:06 +0700

    fix

--- middleware/LoginRateLimit.js
+++ middleware/LoginRateLimit.js
@@ -1,10 +1,14 @@
-const sendEmail = require("../utils/email/sendEmail");
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 60 minutes
 	max: 5,
-	message: { error: 'Too many login attempts. You have been locked for 60 minutes.' }
+	message: {
+		error: `Too many login attempts. You have been locked for 60 minutes. Next login at ${new Date(Date.now() + 60 * 60 * 1000).toLocaleString('en-US', {
+			timeZone: 'Asia/Jakarta'
+		})
+			}.`
+	}
 });
 
 module.exports = limiter;
