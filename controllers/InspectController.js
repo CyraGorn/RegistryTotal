4ba6560e6ddf1a52d98b08727b2c3f017c06f962
Ngo Quy Bao <102130555+CyraGorn@users.noreply.github.com>
2023-06-13 16:50:13 +0700

    Update InspectController.js

--- controllers/InspectController.js
+++ controllers/InspectController.js
@@ -1,11 +1,14 @@
 const RegistryModel = require('../models/Registry');
 const CarsModel = require('../models/Cars');
 const OwnerModel = require('../models/CarOwners');
+const StaffModel = require('../models/Staff');
 const InspectionCalculation = require('../utils/InspectionCalculate');
 const ObjectId = require('mongoose').Types.ObjectId;
 
 class InspectController {
     static getById(req, res) {
+        // lấy thông tin đăng kiểm (thông tin xe, chủ xe, ngày đăng kiểm
+        // ngày hết hạn, nhân viên đăng kiểm) qua id (regisNum)
         RegistryModel.findOne({
             regisNum: req.params.id
         }).select("regisStaff regisPlace regisNum car regisDate expiredDate").populate([{
@@ -26,7 +29,7 @@ class InspectController {
         });
     }
 
-    static getCarByPlate(req, res) {
+    static getCarByPlate(req, res) { // lấy thông tin xe, chủ xe thông qua biển xe
         let plate = req.body.plate;
         CarsModel.findOne({
             numberPlate: plate
@@ -47,22 +50,27 @@ class InspectController {
         })
     }
 
-    static async createInspection(req, res) {
+    static async createInspection(req, res) { // tạo đăng kiểm mới
         let alldata = req.alldata;
         let result = req.result;
+
+        // kiểm tra chủ xe đã có trong db chưa, nếu chưa thì thêm vào
         let existOwner = await InspectionCalculation.insertOwner(alldata['owner']);
         if (!existOwner) {
             return res.status(500).json("SERVER UNAVAILABLE");
         }
         alldata['owner'] = existOwner['_id'];
+
+        // kiểm tra xe đã có trong db chưa, nếu chưa thì thêm vào
         let existCar = await InspectionCalculation.insertCar(alldata);
         if (!existCar) {
             return res.status(500).json("SERVER UNAVAILABLE");
         }
+
+        // tính toán ngày hết hạn dựa vào các thông tin của xe
         let expiredDate = InspectionCalculation.generateExpireDate(alldata);
         let regisNum = await InspectionCalculation.generateRegisNum();
         if (!regisNum) {
-            console.log(1234)
             return res.status(500).json("SERVER UNAVAILABLE");
         }
         let newReg = RegistryModel.create({
@@ -83,14 +91,22 @@ class InspectController {
             }).catch((err) => {
                 return res.status(500).json("SERVER UNAVAILABLE");
             })
+            await StaffModel.updateOne({
+                _id: result['id']
+            }, {
+                $push: {
+                    registed: id
+                }
+            }).catch((err) => {
+                return res.status(500).json("SERVER UNAVAILABLE");
+            })
             return res.status(200).json({
                 status: "SUCCEEDED",
                 regisNum: regisNum
             });
         }).catch((err) => {
-            console.log(err)
             return res.status(500).json("SERVER UNAVAILABLE");
-        })
+        });
     }
 }
 
